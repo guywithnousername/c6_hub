e10d7a3af1393f3385b283e45fcd614064137194
Minglang Du <minglangdu@gmail.com>
2023-09-17 20:13:26 -0700

    make user-related forms look better

--- hub/settings.py
+++ hub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yr2m)zus36jjx=_fs0!)raplh%)#f)95p0_^(!w(9=+5*9qgn6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['c6.cydu.net']
+ALLOWED_HOSTS = ['c6.cydu.net', '127.0.0.1', 'localhost']
 
 
 # Application definition
